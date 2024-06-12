4c00839c13f6d8c7b72b4ab9693fb875b629a051
Luke Finch <lfinch@joriehc.com>
2024-06-12 09:32:37 -0500

    running bot via main

--- main.py
+++ main.py
@@ -5,39 +5,72 @@
 # Imports
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class ExcelHandling:
+    
+    def getDF(excelDataset):
+        df = pd.read_excel(excelDataset)
+        return df
+    
+class BrowserHandling:
+
+    def driverSetUp(webLink):
+        # setting up WebDriver to interact with given website in Edge browser
+        driver = webdriver.Edge(service=Service(r"C:\Users\LukeFinch\Downloads\edgedriver_win64\msedgedriver.exe"))
+        driver.get(webLink)
+
+        return driver
+
+    def fillOutForm(df, driver):
+        ### Have to login first:
+
+        # Username
+        driver.find_element(By.NAME, "username").send_keys("maria")
+
+        # Password
+        driver.find_element(By.NAME, "password").send_keys("thoushallnotpass")
+
+        # Click Log In
+        driver.find_element(By.XPATH, "//button[contains(text(),'Log in')]").click()
+        
+
+        ### Now we can fill out the form for each row in the Excel sheet
+        for index, row in df.iterrows():
+            # Creating variables for each entry in a row
+            fn = row['First Name']
+            ln = row['Last Name']
+            target = str(row['Sales Target'])
+            result = row['Sales Result']
+
+            # Typing info into form
+            WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "firstname")))
+            driver.find_element(By.NAME, "firstname").send_keys(fn)
+            driver.find_element(By.NAME, "lastname").send_keys(ln)
+            driver.find_element(By.NAME, "salesresult").send_keys(result)
+            
+            # Selecting from dropdown menu for Sales Target entry
+            select = Select(driver.find_element(By.ID, "salestarget"))
+            select.select_by_value(target)
+
+            # Submitting form
+            driver.find_element(By.XPATH, "//button[contains(text(),'Submit')]").click()
+
+        driver.quit()
+        
+
 
 def main():
-    # Imported the excel file as a CSV file so I can iterate through it
-    df = pd.read_excel('SalesData.xlsx')
+   df = ExcelHandling.getDF("SalesData.xlsx")
+   driver = BrowserHandling.driverSetUp("https://robotsparebinindustries.com/")
+
+   BrowserHandling.fillOutForm(df, driver)
 
 
-    # setting up WebDriver to interact with given website
-    driver = webdriver.Chrome(service=Service('path_to_chromedriver'))
-    driver.get('https://robotsparebinindustries.com/#/')
-
-    for row in df:
-        # Creating variables for each entry in a row
-        fn = row['First Name']
-        ln = row['Last Name']
-        target = '$' + row['Sales Target']
-        result = row['Sales Result']
-
-        # Typing info into form
-        driver.find_element(By.NAME, "First Name").send_keys(fn)
-        driver.find_element(By.NAME, "Last Name").send_keys(ln)
-        driver.find_element(By.NAME, "Sales Result ($)").send_keys(result)
-        
-        # Selecting from dropdown menu for Sales Target entry
-        select = Select(driver.find_element(By.NAME, "Sales Target ($)"))
-        select.select_by_visible_text(target)
-
-        # Submitting form
-        driver.find_element(By.NAME, "SUBMIT").click()
-
-    driver.quit()
 
 if __name__ == "__main__":
     main()
